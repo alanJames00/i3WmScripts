e4e3bbfd8f97cfeb3e34314f3d1c2ebcad6f891b
alan <alanjames6282@gmail.com>
2024-02-15 20:28:47 +0530

    add trackpad toggle script

---
+++ trackpad_toggle_script.sh
@@ -0,0 +1,23 @@
+
+# xinput device id of trackpad
+trackpad_id=11
+
+# Get current trackpad status
+function get_trackpad_status {
+    st=$(xinput list-props $trackpad_id | grep "Device Enabled" | grep -o "[01]$" )
+    
+    # trackpad status can be 1 or 0 based on whether it is enabled or not
+    echo $st
+}
+
+
+# Main function
+status=$(get_trackpad_status)
+
+case $status in
+
+    1)
+        # currently enabled so disable
+    ;;
+
+esac
